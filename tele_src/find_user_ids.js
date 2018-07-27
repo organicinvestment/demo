df46db7d9080073ded78574289a54f849ade4c6a
organicinvestment <organicinvestmentltd@gmail.com>
2018-07-27 20:07:11 +0530

    tokebn add

--- tele_src/find_user_ids.js
+++ tele_src/find_user_ids.js
@@ -1,6 +1,6 @@
 var TelegramBot = require('./my_ntba');
 
-var TELE_TOKEN = 'YOUR_TELEGRAM_TOKEN';
+var TELE_TOKEN = '679536463:AAHtTgZcA0JdoZAxqPeSM9QcDEicDnsTxWI';
 
 var bot = new TelegramBot(TELE_TOKEN, {polling: true});
 bot.on('message', (msg) => {
